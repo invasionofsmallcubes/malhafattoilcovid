82a80345a45f1f225e95deeb3b894ff07c0f745a
Emanuele Ianni <emanuele.ianni@maersk.com>
2020-10-13 20:24:54 +0200

    player list

--- scripts/cache.js
+++ scripts/cache.js
@@ -4,13 +4,18 @@ const path = require('path')
 
 const players = [];
 
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase()
+}
+
 async function postData() {
   return new Promise((resolve, reject) => {
   const file = path.join(process.cwd(), 'fantacovid.csv')
   return fs.createReadStream(file)
   .pipe(csv())
   .on('data', (row) => {
-    players.push({name: row[0], covid: row[2], team: row[1]});
+    players.push({name: capitalize(row[0]), covid: row[2], team: row[1]});
   })
   .on('end', () => resolve());
 })
